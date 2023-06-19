786eb27237d1fb7de4c4662303024d7f283c3ff4
Ziwei Huang <huang-ziwei@outlook.com>
2023-06-19 16:08:04 +0200

    change: style;

--- static/luvdb/js/text-input-shortcuts.js
+++ static/luvdb/js/text-input-shortcuts.js
@@ -1,4 +1,5 @@
 document.getElementById('text-input').addEventListener('keydown', function(e) {
+    // inserting four spaces with Tab
     if (e.key == 'Tab') {
         e.preventDefault();
         var start = this.selectionStart;
@@ -12,7 +13,7 @@ document.getElementById('text-input').addEventListener('keydown', function(e) {
         this.selectionStart = this.selectionEnd = start + 4;
     }
 
-    // Add new shortcut for submitting form with Ctrl+Enter (or Command+Enter on Mac)
+    // submitting form with Ctrl+Enter (or Command+Enter on Mac)
     if ((e.ctrlKey || e.metaKey) && (e.key == 'Enter' || e.code == 'Enter')) {
         e.preventDefault();
 
@@ -26,4 +27,22 @@ document.getElementById('text-input').addEventListener('keydown', function(e) {
             elem = elem.parentElement;
         }
     }
+
+    // first diable browser back navigation with Ctrl+Z (or Command+Z on Mac) in the text-input field
+    // then add undo delete with Ctrl+Z (or Command+Z on Mac) in the text-input field
+    if ((e.ctrlKey || e.metaKey) && (e.key == 'z' || e.code == 'KeyZ')) {
+        e.preventDefault();
+        if (e.shiftKey) {
+            document.execCommand('redo', false);
+        } else {
+            document.execCommand('undo', false);
+        }
+    }
+
+    // add redo delete with Ctrl+Shift+Z (or Command+Shift+Z on Mac) in the text-input field
+    if ((e.ctrlKey || e.metaKey) && (e.shiftKey) && (e.key == 'z' || e.code == 'KeyZ')) {
+        e.preventDefault();
+        document.execCommand('redo', false);
+    }
+
 });
