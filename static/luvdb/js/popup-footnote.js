96251efe93d5af7d8ba1bdb676b77bad52f24fe4
Ziwei Huang <huang-ziwei@outlook.com>
2023-07-26 13:22:55 +0200

    update: popup-note css and js

--- static/luvdb/js/popup-footnote.js
+++ static/luvdb/js/popup-footnote.js
@@ -19,6 +19,40 @@ for (var i = 0; i < refs.length; i++) {
     footnoteSpan.setAttribute('class', "popupnote");
     refs[i].setAttribute('class', 'popupnote-parent');
 
+    // Remove element with class 'footnote-backref' 
+    var backref = footnoteSpan.getElementsByClassName('footnote-backref');
+    if (backref[0]) {
+        backref[0].remove();
+    }
+
     // Append the footnote to the footnote reference
     refs[i].appendChild(footnoteSpan);
 }
+
+
+var popups = document.getElementsByClassName('popupnote');
+
+for (var i = 0; i < popups.length; i++) {
+  popups[i].onmouseenter = function() {
+    // Get bounding rectangle of the popup.
+    var rect = this.getBoundingClientRect();
+    // If it's out of the left boundary of the viewport.
+    if(rect.left < 0) {
+      this.style.left = '0';
+      this.style.transform = 'translateX(0)';
+    }
+
+    // If it's out of the right boundary of the viewport.
+    else if(rect.right > window.innerWidth) {
+      var overflowAmount = rect.right - window.innerWidth;
+      this.style.left = `calc(0% - ${overflowAmount}px)`;
+    }
+  }
+}
+
+
+
+
+
+
+
