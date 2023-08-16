67a7ab779653913670d61fe95b0cb2de05167e94
Ziwei Huang <huang-ziwei@outlook.com>
2023-08-16 17:24:27 +0200

    add: templatetags for status; formatting: use djlint

--- activity_feed/templatetags/parse_activity_type.py
+++ activity_feed/templatetags/parse_activity_type.py
@@ -17,3 +17,40 @@ def parse_activity_type(value):
         return "checked in"
     else:
         return value
+
+
+STATUS_CLASSES = {
+    # Reading Status
+    "to_read": "text-bg-info",
+    "reading": "text-bg-primary",
+    "finished_reading": "text-bg-success",
+    "rereading": "text-bg-primary",
+    "reread": "text-bg-success",
+    # Watching Status
+    "to_watch": "text-bg-info",
+    "watching": "text-bg-primary",
+    "watched": "text-bg-success",
+    "rewatching": "text-bg-primary",
+    "rewatched": "text-bg-success",
+    # Listening Status
+    "to_listen": "text-bg-info",
+    "looping": "text-bg-primary",
+    "listening": "text-bg-primary",
+    "listened": "text-bg-success",
+    "relistening": "text-bg-primary",
+    "relistened": "text-bg-success",
+    # Playing Status
+    "to_play": "text-bg-info",
+    "currently_playing": "text-bg-primary",
+    "played": "text-bg-success",
+    "replaying": "text-bg-primary",
+    "replayed": "text-bg-success",
+    # Shared Statuses
+    "paused": "text-bg-warning",
+    "abandoned": "text-bg-danger",
+}
+
+
+@register.filter(name="get_status_class")
+def get_status_class(status):
+    return STATUS_CLASSES.get(status, "text-bg-default")
