db5b9d8afaf0c90440e685734eaf64a643418705
Ziwei Huang <huang-ziwei@outlook.com>
2023-10-12 10:38:10 +0200

    change: change urls for discover/models

--- discover/urls.py
+++ discover/urls.py
@@ -11,11 +11,11 @@ from .views import (
 
 app_name = "discover"
 urlpatterns = [
-    path("all", DiscoverListAllView.as_view(), name="all"),
-    path("lists", DiscoverLuvListListView.as_view(), name="lists"),
-    path("posts", DiscoverPostListView.as_view(), name="posts"),
-    path("pins", DiscoverPinListView.as_view(), name="pins"),
-    path("liked", DiscoverLikedView.as_view(), name="liked"),
+    path("all/", DiscoverListAllView.as_view(), name="all"),
+    path("lists/", DiscoverLuvListListView.as_view(), name="lists"),
+    path("posts/", DiscoverPostListView.as_view(), name="posts"),
+    path("pins/", DiscoverPinListView.as_view(), name="pins"),
+    path("liked/", DiscoverLikedView.as_view(), name="liked"),
     path(
         "vote/<str:content_type>/<int:object_id>/<str:vote_type>/",
         vote,
