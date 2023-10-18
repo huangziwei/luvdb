748b2293c3c29955b7fca19bf9d9888e3b647ea4
Ziwei Huang <huang-ziwei@outlook.com>
2023-10-18 20:01:39 +0200

    add: 500 template

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,12 @@
+import sys
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import redirect_to_login
 from django.contrib.sitemaps.views import sitemap
-from django.http import Http404
+from django.http import Http404, HttpResponseServerError
+from django.template import loader
 from django.urls import include, path
 
 from accounts.views import search_view
@@ -34,6 +37,13 @@ def custom_admin_login(request):
     raise Http404
 
 
+def custom_500_view(request, *args, **argv):
+    template = loader.get_template("500.html")
+    type_, value, traceback = sys.exc_info()
+
+    return HttpResponseServerError(template.render({"error_message": str(value)}))
+
+
 urlpatterns = [
     path("admin/login/", custom_admin_login, name="custom_admin_login"),
     path("admin/", admin.site.urls),
@@ -62,3 +72,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = "django.views.defaults.page_not_found"
+handler500 = custom_500_view
