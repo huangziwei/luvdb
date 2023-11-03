b23a4c0b87b983bfb1ca1f1eeea54cbf21821004
Ziwei Huang <huang-ziwei@outlook.com>
2023-11-03 20:56:10 +0100

    change: set `performer` as default role for music releases; update generalFormsetHandler.js

--- static/luvdb/js/generalFormsetHandler.js
+++ static/luvdb/js/generalFormsetHandler.js
@@ -1,79 +1,79 @@
-document.addEventListener("DOMContentLoaded", function() {
-
+document.addEventListener("DOMContentLoaded", function () {
     function handleFormset(formsetId, buttonClass) {
-        var formsetRows = document.querySelectorAll(formsetId + ' .form-wrapper');
+        var formsetRows = document.querySelectorAll(formsetId + " .form-wrapper");
         var lastVisibleRow = null;
 
-        formsetRows.forEach(function(row, index) {
-            var selects = row.querySelectorAll('select');
+        formsetRows.forEach(function (row, index) {
             var inputs = row.querySelectorAll('input[type="text"]');
-            var hasSelectedOption = Array.from(selects).some(function(select) {
-                return select.selectedOptions.length > 0 && select.selectedOptions[0].value != '';
-            });
-            var hasInputData = Array.from(inputs).some(function(input) {
-                return input.value.trim() !== '';
+            var creatorSelect = row.querySelector('select[name$="creator"]');
+            var hasCreatorSelected =
+                creatorSelect &&
+                creatorSelect.selectedOptions.length > 0 &&
+                creatorSelect.selectedOptions[0].value != "";
+            var hasInputData = Array.from(inputs).some(function (input) {
+                return input.value.trim() !== "";
             });
 
-            var shouldHide = index > 0 && !hasSelectedOption && !hasInputData;
+            var shouldHide = index > 0 && !hasCreatorSelected && !hasInputData;
 
             if (formsetId === "#regionreleasedates") {
                 shouldHide = index > 0 && !hasInputData;
             }
 
             if (formsetId === "#release-track-formset") {
-                shouldHide = index > 0 && !hasSelectedOption;
+                shouldHide = index > 0 && !hasCreatorSelected;
             }
 
             if (shouldHide) {
-                row.style.visibility = 'hidden';
-                row.style.height = '0';
-                row.classList.add('bg-light');
+                row.style.visibility = "hidden";
+                row.style.height = "0";
+                row.classList.add("bg-light");
             } else {
                 lastVisibleRow = row;
-                row.classList.add('bg-light', 'p-2', 'mb-3');
+                row.classList.add("bg-light", "p-2", "mb-3");
             }
         });
 
         // Add the "+" button to the last visible row only
-        var buttonDiv = document.createElement('div');
-        buttonDiv.className = 'col-md-1 mb-3';
-        var innerDiv = document.createElement('div');
-        innerDiv.className = 'mb-1';
-        var label = document.createElement('label');
-        label.className = 'form-label';
-        label.textContent = 'Add';
-        var button = document.createElement('button');
-        button.type = 'button';
-        button.textContent = '+';
-        button.className = 'btn btn-sm btn-outline-secondary ' + buttonClass;
+        var buttonDiv = document.createElement("div");
+        buttonDiv.className = "col-md-1 mb-3";
+        var innerDiv = document.createElement("div");
+        innerDiv.className = "mb-1";
+        var label = document.createElement("label");
+        label.className = "form-label";
+        label.textContent = "Add";
+        var button = document.createElement("button");
+        button.type = "button";
+        button.textContent = "+";
+        button.className = "btn btn-sm btn-outline-secondary " + buttonClass;
 
         innerDiv.appendChild(label);
         innerDiv.appendChild(button);
         buttonDiv.appendChild(innerDiv);
         lastVisibleRow.lastElementChild.appendChild(buttonDiv);
 
-        document.querySelector('.' + buttonClass).addEventListener('click', function() {
+        document.querySelector("." + buttonClass).addEventListener("click", function () {
             var currentRow = this.parentElement.parentElement.parentElement.parentElement;
             var nextHiddenRow = currentRow.nextElementSibling;
             if (nextHiddenRow) {
-                nextHiddenRow.style.visibility = 'visible';
-                nextHiddenRow.style.height = 'auto';
-                nextHiddenRow.classList.add('mb-3', 'p-2');
+                nextHiddenRow.style.visibility = "visible";
+                nextHiddenRow.style.height = "auto";
+                nextHiddenRow.classList.add("mb-3", "p-2");
 
                 // Add the "+" button to the next row
                 var buttonDivClone = buttonDiv.cloneNode(true);
                 nextHiddenRow.lastElementChild.appendChild(buttonDivClone);
-                var addButton = buttonDivClone.querySelector('button');
-                addButton.addEventListener('click', arguments.callee);
+                var addButton = buttonDivClone.querySelector("button");
+                addButton.addEventListener("click", arguments.callee);
             }
 
             // Remove the "+" button from the current row
             this.parentElement.parentElement.remove();
 
             // Hide the add links in the current row
-            var addLinks = currentRow.querySelectorAll('a.text-secondary');
-            addLinks.forEach(function(link) {
-                link.style.display = 'none';
+            var addLinks = currentRow.querySelectorAll("a.text-secondary");
+            addLinks.forEach(function (link) {
+                link.style.display = "none";
             });
         });
     }
