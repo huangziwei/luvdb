726cdc4d743316fb3fbcce5edf8b892bfbc6c0b0
Ziwei Huang <huang-ziwei@outlook.com>
2023-11-08 13:39:27 +0100

    change: rid special bio color

--- static/luvdb/js/dark-mode.js
+++ static/luvdb/js/dark-mode.js
@@ -27,16 +27,12 @@ document.addEventListener("DOMContentLoaded", function () {
             metaThemeColor.setAttribute("content", "#2B3035");
 
             bgLightElements.forEach((el) => el.classList.add("bg-dark-highlight"));
-            // bgWhiteElements.forEach(el => el.classList.replace('bg-white', 'bg-dark'));
-            bioElements.forEach((el) => (el.style.backgroundColor = "#2B3035")); // Dark mode compatible color for bio
             faIcons.forEach((el) => el.setAttribute("fill", "#ccc")); // Dark mode color for FontAwesome icons
             stickyNoteElements.forEach((el) => el.classList.add("bg-dark"));
         } else {
             htmlElement.removeAttribute("data-bs-theme");
             bgLightElements.forEach((el) => el.classList.remove("bg-dark-highlight"));
             metaThemeColor.setAttribute("content", "#F6F7F9");
-            // bgWhiteElements.forEach(el => el.classList.replace('bg-dark', "bg-white"));
-            bioElements.forEach((el) => (el.style.backgroundColor = "#e5ebef")); // Original color for bio
             faIcons.forEach((el) => el.setAttribute("fill", "#6C757D")); // Original color for FontAwesome icons
             stickyNoteElements.forEach((el) => el.classList.remove("bg-dark"));
         }
