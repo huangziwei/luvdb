287152c4c8a7d772ab76861019df1b513b7cfcb9
Ziwei Huang <huang-ziwei@outlook.com>
2023-12-07 15:41:18 +0100

    add: gzip middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,6 +80,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.gzip.GZipMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
