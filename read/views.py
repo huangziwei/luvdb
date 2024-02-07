e700659262b814f211d8407dee0c2e1ef96f46da
Ziwei Huang <huang-ziwei@outlook.com>
2024-02-07 14:12:30 +0100

    change: order issues by publication time

--- read/views.py
+++ read/views.py
@@ -953,6 +953,7 @@ class PeriodicalDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["issues"] = self.object.issues.all().order_by("publication_date")
         # contributors
         context["contributors"] = get_contributors(self.object)
         return context
