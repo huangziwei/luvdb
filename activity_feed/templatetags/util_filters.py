57a8bb6480609e22bd6a0801c3c044ccadf27f05
Ziwei Huang <huang-ziwei@outlook.com>
2024-05-16 23:22:36 +0200

    change: list pagination style

--- activity_feed/templatetags/util_filters.py
+++ activity_feed/templatetags/util_filters.py
@@ -29,3 +29,15 @@ def extract_year(value):
 def daysince(timestamp):
     """Returns the number of days since the given timestamp."""
     return (timezone.now() - timestamp).days
+
+
+@register.filter(name="parse_range")
+def parse_range(value):
+    """Parses a range string into a list of integers."""
+    if value is None:
+        return []
+
+    if "-" in value:
+        return value.split("-")[0]
+    else:
+        return value
